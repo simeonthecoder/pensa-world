9c58892d415b2781fcefc2831a5d32344e8b3aed
SimeonTheCoder <69637412+SimeonTheCoder@users.noreply.github.com>
2024-01-22 22:07:02 +0200

    Initial commit

---
+++ TeamBeyond/Assets/Scripts/Ragdoll.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll : MonoBehaviour
+{
+    private Vector3 previous;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        previous = transform.position;
+    }
+
+    void Update() {
+        if(Input.GetKeyDown("r"))
+        {
+            GetComponent<Animator>().enabled = !GetComponent<Animator>().enabled;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log((transform.position - previous).magnitude);
+
+        if((transform.position - previous).magnitude >= 0.25) {
+            GetComponent<Animator>().enabled = false;
+        }
+
+        previous = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+}
