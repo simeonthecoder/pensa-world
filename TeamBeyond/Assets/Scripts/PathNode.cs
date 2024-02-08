70a04fd061936e72b782d0785ec6ab0295ec2c14
SimeonTheCoder <69637412+SimeonTheCoder@users.noreply.github.com>
2024-02-08 13:07:52 +0200

    General polish. QOL improvements.

--- TeamBeyond/Assets/Scripts/PathNode.cs
+++ TeamBeyond/Assets/Scripts/PathNode.cs
@@ -13,9 +13,17 @@ public class PathNode : MonoBehaviour
 
     public Vector3 offset;
 
+    public bool waving;
+
     // Start is called before the first frame update
     void Start()
     {
+        if(waving)
+        {
+            Destroy(this.gameObject.GetComponent<MeshRenderer>());
+        }
+
+        waving = !waving;
         pathManager = GameObject.Find("Path Manager").GetComponent<PathManager>();
     }
 
@@ -23,7 +31,15 @@ public class PathNode : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime;
-        this.transform.localScale = new Vector3(Mathf.Sin(time) / 2 + 1, Mathf.Sin(time) / 2 + 1, Mathf.Sin(time) / 2 + 1);
+
+        if(waving)
+        {
+            this.transform.localScale = new Vector3(
+                (Mathf.Sin(time) / 2 + 1.5f),
+                (Mathf.Sin(time) / 2 + 1.5f),
+                (Mathf.Sin(time) / 2 + 1.5f)
+            );
+        }
     }
 
     void OnMouseDown()
@@ -31,13 +47,36 @@ public class PathNode : MonoBehaviour
         pathManager.camera.GetComponent<DotNavigator>().Move(this.gameObject.transform.position + offset);
         pathManager.camera.GetComponent<DotNavigator>().dots.Push(this.gameObject);
 
-        Color oldColor = this.gameObject.GetComponent<MeshRenderer>().material.color;
-        Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b, 0);
-        this.gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", newColor);
+        if(waving)
+        {
+            Color oldColor = this.gameObject.GetComponent<MeshRenderer>().material.color;
+            Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b, 0);
+            this.gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", newColor);
+        }
 
         for(int i = 0; i < linkedDoors.Length; i ++)
         {
             linkedDoors[i].Toggle();
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        if(waving)
+        {
+            Color oldColor = this.gameObject.GetComponent<MeshRenderer>().material.color;
+            Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b, 0.3f);
+            this.gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", newColor);
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if(waving)
+        {
+            Color oldColor = this.gameObject.GetComponent<MeshRenderer>().material.color;
+            Color newColor = new Color(oldColor.r, oldColor.g, oldColor.b, 0f);
+            this.gameObject.GetComponent<MeshRenderer>().material.SetColor("_Color", newColor);
         }
     }
 }
