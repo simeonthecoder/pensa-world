30790646cd5332ed325f92d9c79db4142a51a369
Kris <Kristianhristov10@gmail.com>
2024-06-08 23:05:00 +0300

    fishing_pull

--- TeamBeyond/Assets/Scripts/TriggerHandler.cs
+++ TeamBeyond/Assets/Scripts/TriggerHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TriggerHandler : MonoBehaviour
 {
@@ -7,12 +8,17 @@ public class TriggerHandler : MonoBehaviour
     public GameObject targetPosition;
     private bool inside = false;
     private bool active = false;
-
+    public Text uiText;
     public float cooldown;
-    private float time = 0;
+
+
+    public TextController textController;
+
+    private float fishing_lenght;
 
     public void Start()
     {
+        randomValue = Random.Range(4f, 20f);
         Debug.Log("starting niga");
         rod.SetActive(false);
     }
@@ -57,7 +63,18 @@ public class TriggerHandler : MonoBehaviour
 
             if (time > cooldown)
             {
+                fishing_lenght--;
                 rod.GetComponent<Animator>().SetBool("rod_throw", false);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && fishing_lenght <= 0)
+            {
+
+                textController.ChangeText("Press SPACE");
+                rod.GetComponent<Animator>().SetBool("rod_pull", true);
+
+                randomValue = Random.Range(4f, 20f);
+
             }
         }
 
