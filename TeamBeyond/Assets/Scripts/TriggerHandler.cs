0ccd8482514fe6c15d038c97d3006c6fd1f11384
SimeonTheCoder <69637412+SimeonTheCoder@users.noreply.github.com>
2024-06-11 15:03:15 +0300

    General polish. Added pub outside 3D model

--- TeamBeyond/Assets/Scripts/TriggerHandler.cs
+++ TeamBeyond/Assets/Scripts/TriggerHandler.cs
@@ -14,8 +14,6 @@ public class TriggerHandler : MonoBehaviour
     private float time = 0f;
     private float randomValue = 0f;
 
-    public TextController textController;
-
     private float fishing_lenght;
 
     public void Start()
@@ -71,8 +69,7 @@ public class TriggerHandler : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.Space) && fishing_lenght <= 0)
             {
-
-                textController.ChangeText("Press SPACE");
+                uiText.text = "Press SPACE";
                 rod.GetComponent<Animator>().SetBool("rod_pull", true);
 
                 randomValue = Random.Range(4f, 20f);
